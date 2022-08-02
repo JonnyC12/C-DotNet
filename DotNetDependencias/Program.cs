e2b01f9d262a3bd8a0a49c978bd8cf12d2593d44
JonnyC12 <jonathan.coque@edu.uah.es>
2022-08-02 10:51:16 +0200

    primeros pasos en C#/.Net

---
+++ DotNetDependencias/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+// esto es una prueba de como se instala paquetes en dotnet.
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+}
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+int a = 2;
+for (int i = a; i < 4; i++)
+{
+    Console.WriteLine(i);
+}
