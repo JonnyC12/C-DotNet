7e9e746c8320953fe2a135cb769b4ddf0fc92a2e
JonnyC12 <jonathan.coque@edu.uah.es>
2022-08-17 16:12:39 +0200

    prueba

--- DotNetMinApiWithEF/PizzaStore/Program.cs
+++ DotNetMinApiWithEF/PizzaStore/Program.cs
@@ -9,10 +9,11 @@ async Task<String> SaludoAsyn()
     return "saludos Asincrono";
 }
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
 // agregando entityframework 
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
